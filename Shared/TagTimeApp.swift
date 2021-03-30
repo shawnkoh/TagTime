ecdc6f07aa4b446bdbc9c322fa3b7a42d98339b5
Shawn Koh <shawnkoh@me.com>
2021-03-31 01:51:40 +0800

    Initial Commit

---
+++ Shared/TagTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TagTimeApp.swift
+//  Shared
+//
+//  Created by Shawn Koh on 31/3/21.
+//
+
+import SwiftUI
+
+@main
+struct TagTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
