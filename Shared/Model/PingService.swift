ab298ebf5d8655a658cfa63ef137194bff439b85
Shawn Koh <shawnkoh@me.com>
2021-04-10 17:44:59 +0800

    Add Png data structure to replace Ping

--- Shared/Model/PingService.swift
+++ Shared/Model/PingService.swift
@@ -10,6 +10,48 @@ import FirebaseFirestore
 import FirebaseFirestoreSwift
 import Firebase
 
+struct Png {
+    let seed: Int
+    let unixtime: Int
+
+    var date: Date {
+        Date(timeIntervalSince1970: TimeInterval(unixtime))
+    }
+
+    // =7^5: Multiplier for LCG random number generator
+    private let ia = 16807
+    // =2^31-1: Modulus used for the RNG
+    private let im = 2147483647
+
+    init(seed: Int = Self.tagTimeBirthSeed, unixtime: Int = Self.tagTimeBirthTime) {
+        self.seed = seed
+        self.unixtime = unixtime
+    }
+
+    /// Returns the next ping.
+    /// averagePingInterval: Average gap between pings, in seconds
+    func nextPing(averagePingInterval: Int) -> Png {
+        // Linear Congruential Generator, returns random integer in {1, ..., IM-1}.
+        // This is ran0 from Numerical Recipes and has a period of ~2 billion.
+        // lcg()/IM is a U(0,1) R.V.
+        let seed = ia * self.seed % im
+
+        // Return a random number drawn from an exponential distribution with mean
+        let exprand = Double(-averagePingInterval) * log(Double(seed) / Double(im))
+
+        // Every TagTime gap must be an integer number of seconds not less than 1
+        let gap = Int(max(1, round(exprand)))
+        let unixtime = self.unixtime + gap
+
+        return Png(seed: seed, unixtime: unixtime)
+    }
+}
+
+extension Png {
+    static let tagTimeBirthTime = 1184097393
+    static let tagTimeBirthSeed = 11193462
+}
+
 final class Randomer {
     // Average gap between pings, in seconds
     private let averagePingInterval: Int
