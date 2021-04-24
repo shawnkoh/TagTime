74f913501bb071794ce53a920b5c8917cc74253c
Shawn Koh <shawn@shawnkoh.sg>
2021-04-25 03:04:47 +0800

    Refactor DebugMenu to use view builder

--- Shared/Views/Preferences/DebugMenu.swift
+++ Shared/Views/Preferences/DebugMenu.swift
@@ -9,41 +9,36 @@
 import SwiftUI
 
 struct DebugMenu: View {
+    @ViewBuilder
+    private func button(_ text: String, action: @escaping () -> Void) -> some View {
+        Button(action: action) {
+            HStack {
+                Spacer()
+                Text(text)
+                    .foregroundColor(.primary)
+                    .padding()
+                Spacer()
+            }
+            .background(Color.hsb(223, 69, 90))
+            .cornerRadius(8)
+        }
+    }
+
     var body: some View {
         VStack(alignment: .leading) {
             Text("Debug Mode")
                 .font(.title)
                 .bold()
 
-            Button(action: {
+            button("Schedule notification in 7 seconds") {
                 NotificationService.shared.scheduleNotification(
                     ping: .init(timeIntervalSinceNow: 7),
                     badge: AnswerService.shared.unansweredPings.count + 1
                 )
-            }) {
-                HStack {
-                    Spacer()
-                    Text("Schedule notification in 7 seconds")
-                        .foregroundColor(.primary)
-                        .padding()
-                    Spacer()
-                }
-                .background(Color.hsb(223, 69, 90))
-                .cornerRadius(8)
             }
 
-            Button(action: {
+            button("Delete all answers") {
                 AnswerService.shared.deleteAllAnswers()
-            }) {
-                HStack {
-                    Spacer()
-                    Text("Delete all answers")
-                        .foregroundColor(.primary)
-                        .padding()
-                    Spacer()
-                }
-                .background(Color.hsb(223, 69, 90))
-                .cornerRadius(8)
             }
         }
     }
