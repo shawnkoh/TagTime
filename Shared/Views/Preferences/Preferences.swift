446ee7434f5b7cab14387962e0c990ea6419dc4c
Shawn Koh <shawn@shawnkoh.sg>
2021-05-03 17:10:03 +0800

    Add ability to link account with Beeminder

--- Shared/Views/Preferences/Preferences.swift
+++ Shared/Views/Preferences/Preferences.swift
@@ -12,6 +12,23 @@ struct Preferences: View {
 
     private var debugMode = true
 
+    @State private var isAuthenticatingBeeminder = false
+
+    @ViewBuilder
+    private func button(text: String, action: @escaping () -> Void) -> some View {
+        Button(action: action) {
+            HStack {
+                Spacer()
+                Text(text)
+                    .foregroundColor(.primary)
+                    .padding()
+                Spacer()
+            }
+            .background(Color.hsb(223, 69, 98))
+            .cornerRadius(8)
+        }
+    }
+
     var body: some View {
         VStack(alignment: .leading, spacing: 20) {
             PageTitle(title: "Preferences", subtitle: "Suit yourself")
@@ -28,17 +45,9 @@ struct Preferences: View {
                     onCommit: {}
                 )
 
-                Button(action: { FacebookLoginService.shared.login() }) {
-                    HStack {
-                        Spacer()
-                        Text("Login with Facebook")
-                            .foregroundColor(.primary)
-                            .padding()
-                        Spacer()
-                    }
-                    .background(Color.hsb(223, 69, 98))
-                    .cornerRadius(8)
-                }
+                BeeminderLoginButton()
+
+                button(text: "Login with Facebook") { FacebookLoginService.shared.login() }
 
                 #if DEBUG
                 Divider()
