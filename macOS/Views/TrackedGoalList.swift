05622af3560144f412754b79b0dc2d44fad34552
Shawn Koh <shawn@shawnkoh.sg>
2021-05-17 13:27:51 +0800

    Move TrackedGoalListViewModel into its own file

--- macOS/Views/TrackedGoalList.swift
+++ macOS/Views/TrackedGoalList.swift
@@ -8,26 +8,6 @@
 import SwiftUI
 import Resolver
 import Beeminder
-import Combine
-
-final class TrackedGoalListViewModel: ObservableObject {
-    @LazyInjected private var goalService: GoalService
-
-    @Published private(set) var trackedGoals: [Goal] = []
-
-    private var subscribers = Set<AnyCancellable>()
-
-    init() {
-        goalService.goalsPublisher
-            .combineLatest(goalService.goalTrackersPublisher)
-            .map { goals, goalTrackers in
-                goals.filter { goalTrackers[$0.id] != nil }
-            }
-            .receive(on: DispatchQueue.main)
-            .sink { [weak self] in self?.trackedGoals = $0 }
-            .store(in: &subscribers)
-    }
-}
 
 struct TrackedGoalList: View {
     @StateObject private var viewModel = TrackedGoalListViewModel()
