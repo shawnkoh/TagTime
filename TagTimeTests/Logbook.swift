288ff5670be6fb5b94be94b8ced365d4e0d139bf
Shawn Koh <shawn@shawnkoh.sg>
2021-05-19 15:15:57 +0800

    Add generic Array extension for Groups

--- TagTimeTests/Logbook.swift
+++ TagTimeTests/Logbook.swift
@@ -7,6 +7,39 @@
 
 import XCTest
 
+enum Group<Value: Equatable>: Equatable {
+    case single(Value)
+    case multiple([Value])
+}
+
+extension Array where Array.Element: Equatable {
+    func grouped() -> [Group<Array.Element>] {
+        var result: [Group<Array.Element>] = []
+        var cursor = 0
+        while cursor < self.count {
+            let current = self[cursor]
+
+            // find the next index that is not the same
+            var nextIndex = cursor + 1
+            while nextIndex < self.count {
+                if current == self[nextIndex] {
+                    nextIndex += 1
+                } else {
+                    break
+                }
+            }
+
+            if nextIndex == cursor + 1 {
+                result.append(.single(current))
+            } else {
+                result.append(.multiple(Array(self[cursor..<nextIndex])))
+            }
+            cursor = nextIndex
+        }
+        return result
+    }
+}
+
 private enum Row: Equatable {
     case single(String)
     case multiple([String])
