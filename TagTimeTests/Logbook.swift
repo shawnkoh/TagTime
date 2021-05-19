22bacb43a0d6daebbd27f11c345fcb8b582e12c8
Shawn Koh <shawn@shawnkoh.sg>
2021-05-19 14:47:18 +0800

    Add algorithm for rolling up similarities in an array

--- TagTimeTests/Logbook.swift
+++ TagTimeTests/Logbook.swift
@@ -26,16 +26,19 @@ class Logbook: XCTestCase {
 
         test(input: "A", expected: single("A"))
 
-        test(input: "A", "A", expected: multiple("B", "B"))
+        test(input: "A", "A", expected: multiple("A", "A"))
         test(input: "A", "B", expected: single("A"), single("B"))
 
+        test(input: "A", "A", "A", expected: multiple("A", "A", "A"))
         test(input: "A", "A", "B", expected: multiple("A", "A"), single("B"))
         test(input: "A", "B", "B", expected: single("A"), multiple("B", "B"))
+        test(input: "A", "B", "C", expected: single("A"), single("B"), single("C"))
 
         test(input: "A", "A", "B", "B", expected: multiple("A", "A"), multiple("B", "B"))
         test(input: "A", "B", "A", "B", expected: single("A"), single("B"), single("A"), single("B"))
         test(input: "A", "A", "A", "B", expected: multiple("A", "A", "A"), single("B"))
         test(input: "A", "B", "B", "B", expected: single("A"), multiple("B", "B", "B"))
+        test(input: "A", "B", "C", "D", expected: single("A"), single("B"), single("C"), single("D"))
     }
 
     private func test(input: String..., expected: Row...) {
@@ -52,12 +55,28 @@ class Logbook: XCTestCase {
     }
 
     private func row(_ input: [String]) -> [Row] {
-        guard let first = input.first else {
-            return []
-        }
-        var cursor = first
         var result: [Row] = []
-        var multiple: [String] = []
-        return []
+        var cursor = 0
+        while cursor < input.count {
+            let current = input[cursor]
+
+            // find the next index that is not the same
+            var nextIndex = cursor + 1
+            while nextIndex < input.count {
+                if current == input[nextIndex] {
+                    nextIndex += 1
+                } else {
+                    break
+                }
+            }
+
+            if nextIndex == cursor + 1 {
+                result.append(.single(current))
+            } else {
+                result.append(.multiple(Array(input[cursor..<nextIndex])))
+            }
+            cursor = nextIndex
+        }
+        return result
     }
 }
