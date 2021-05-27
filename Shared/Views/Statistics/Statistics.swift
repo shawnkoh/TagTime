79939ce644e03623eaefb74cc2e9d1f8ca42a7be
Shawn Koh <shawn@shawnkoh.sg>
2021-05-27 19:27:09 +0800

    Add preliminary statistics page

--- Shared/Views/Statistics/Statistics.swift
+++ Shared/Views/Statistics/Statistics.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 import Resolver
 
+fileprivate extension Date {
+    var day: StatisticsViewModel.Day {
+        let components = Calendar.current.dateComponents([.day, .month, .year], from: self)
+        return .init(day: components.day!, month: components.month!, year: components.year!)
+    }
+}
+
 struct Statistics: View {
+    @StateObject private var viewModel = StatisticsViewModel()
+
     var body: some View {
         VStack(alignment: .leading) {
             PageTitle(title: "Statistics", subtitle: "Quantified Self")
+
+            Picker("", selection: $viewModel.mode) {
+                ForEach(StatisticsViewModel.Mode.allCases, id: \.self) {
+                    Text($0.rawValue)
+                        .tag($0.rawValue)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+
+            DatePicker("Date", selection: $viewModel.date, in: viewModel.startDate...Date(), displayedComponents: .date)
+
+            if
+                let tags = viewModel.tagCountByDate[viewModel.date.day],
+                let total = viewModel.totalByDay[viewModel.date.day]
+            {
+                List {
+                    Section {
+                        ForEach(Array(tags.keys), id: \.self) { tag in
+                            if let time = tags[tag] {
+                                ProgressView(
+                                    value: Double(time),
+                                    total: Double(total),
+                                    label: { Text(tag) },
+                                    currentValueLabel: { Text("\(time)") }
+                                )
+                            }
+                        }
+                    }
+                }
+                .listStyle(PlainListStyle())
+            }
         }
     }
 }
